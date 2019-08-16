4ea8d60e9ca81aaf0fa5b66cf83f2dc584832148
lsb <lishoubo2014@126.com>
2019-08-17 00:09:33 +0800

    Python Crash 12.7

--- game_functions.py
+++ game_functions.py
@@ -2,20 +2,33 @@ import sys
 
 import pygame
 
+def check_keydown_events(event, ship):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        # 按下->调整移动标志为True，间接控制移动
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        # 按下<-调整移动标志为True，间接控制移动
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """松开响应"""
+    if event.key == pygame.K_RIGHT:
+        # 松开->调整移动标志为False，停止移动
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        # 松开<-调整移动标志为False，停止移动
+        ship.moving_left = False
+
 def check_events(ship):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                #按下->调整移动标志为True，间接控制移动
-                ship.moving_right = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                #松开->调整移动标志为False，停止移动
-                ship.moving_right = False
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图像，并切换到新屏幕"""
--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Settings():
         self.screen_width = 960
         self.screen_height = 640
         self.bg_color = (230, 230, 230)
+
+        # 飞船的设置
+        self.ship_speed_factor = 1.5
