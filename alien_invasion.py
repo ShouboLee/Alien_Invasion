1840dbb4414a23c39558d3580843c15f446b5b28
lsb <lishoubo2014@126.com>
2019-08-18 15:11:35 +0800

    chap 13 finish Alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from game_stats import GameStats
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
@@ -12,26 +13,27 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    stats = GameStats(ai_settings)
 
     #创建一艘飞船
     ship = Ship(ai_settings,screen)
     #创建一个用于存储子弹的编组
     bullets = Group()
+    #创建一个外星人编组
+    aliens = Group()
+    #创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     #开始游戏的主循环
     while True:
-
         #监视键盘和鼠标时间
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
 
-        #删除已消失的子弹
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-        print(len(bullets))
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens,bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, stats, screen, ship, aliens, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         #子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
@@ -17,3 +17,10 @@ class Settings():
 
         # 飞船的设置
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
+
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 100
+        #fleet_direction为1表示右移，为-1表示左移
+        self.fleet_direction = 1
